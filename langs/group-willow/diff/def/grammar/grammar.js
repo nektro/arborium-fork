c5d5318fd08f95a36666f4dc0a9d2c533748fac3
Meghan Denny <hello@nektro.net>
2026-09-07 01:54:35 -0700

    diff: sync to master

--- langs/group-willow/diff/def/grammar/grammar.js
+++ langs/group-willow/diff/def/grammar/grammar.js
@@ -1,6 +1,10 @@
 const NEWLINE = /\r?\n/;
 const WHITE_SPACE = /[\t\f\v ]+/;
 const ANYTHING = /[^\r\n]+/;
+const BASE85_LINE = /[A-Za-z][0-9A-Za-z!#$%&()*+\-;<=>?@^_`{|}~]+/;
+const BASE85 = new RegExp(
+  `${BASE85_LINE.source}(?:${NEWLINE.source}${BASE85_LINE.source})*`
+);
 
 export default grammar({
   name: "diff",
@@ -20,13 +24,17 @@ export default grammar({
         $.binary_change,
         $.index,
         $.similarity,
+        $.dissimilarity,
         $.old_file,
         $.new_file,
         $.location,
         $.addition,
         $.deletion,
+        $.change,
         $.context,
-        $.comment
+        $.comment,
+        $.special,
+        $.unrecognized,
       ),
 
     block: ($) =>
@@ -36,13 +44,48 @@ export default grammar({
           NEWLINE,
           repeat(
             seq(
-              choice($.file_change, $.binary_change, $.index, $.similarity),
+              choice(
+                $.file_change,
+                $.binary_change,
+                $.index,
+                $.similarity,
+                $.dissimilarity
+              ),
               NEWLINE
             )
           ),
-          optional(seq($.old_file, NEWLINE, $.new_file, NEWLINE, $.hunks))
+          optional(
+            choice(
+              seq($.old_file, NEWLINE, $.new_file, NEWLINE, $.hunks),
+              $.binary_patch
+            )
+          )
         )
       ),
+
+    binary_patch: ($) =>
+      prec.right(
+        seq(
+          iseq("GIT", "binary", "patch"),
+          NEWLINE,
+          field("forward", $.binary_hunk),
+          optional(field("reverse", $.binary_hunk))
+        )
+      ),
+
+    binary_hunk: ($) =>
+      prec.right(
+        seq(
+          choice(token.immediate("literal"), token.immediate("delta")),
+          alias(/\d+/, $.size),
+          NEWLINE,
+          $.payload,
+          NEWLINE,
+          prec.right(repeat(NEWLINE))
+        )
+      ),
+
+    payload: ($) => token.immediate(BASE85),
 
     hunks: ($) => prec.right(repeat1($.hunk)),
 
@@ -59,7 +102,16 @@ export default grammar({
       prec.right(
         repeat1(
           seq(
-            choice($.addition, $.deletion, $.context),
+            choice(
+              alias($._hunk_addition, $.addition),
+              alias($._hunk_deletion, $.deletion),
+              $.addition,
+              $.deletion,
+              $.change,
+              $.context,
+              $.special,
+              $.unrecognized
+            ),
             prec.right(repeat1(NEWLINE))
           )
         )
@@ -71,7 +123,7 @@ export default grammar({
       choice(
         seq(choice("new", "deleted"), "file", "mode", $.mode),
         seq(choice("new", "old"), "mode", $.mode),
-        seq("rename", choice("from", "to"), $.filename)
+        seq(choice("rename", "copy"), choice("from", "to"), $.filename)
       ),
 
     binary_change: ($) =>
@@ -80,6 +132,8 @@ export default grammar({
     index: ($) => iseq("index", $.commit, "..", $.commit, optional($.mode)),
 
     similarity: ($) => iseq("similarity", "index", alias(/\d+/, $.score), "%"),
+    dissimilarity: ($) =>
+      iseq("dissimilarity", "index", alias(/\d+/, $.score), "%"),
 
     old_file: ($) => iseq("---", $.filename),
     new_file: ($) => iseq("+++", $.filename),
@@ -87,27 +141,35 @@ export default grammar({
     location: ($) =>
       iseq("@@", $.linerange, $.linerange, "@@", optional(ANYTHING)),
 
+    _hunk_addition: ($) => iseq("+++", ANYTHING),
+    _hunk_deletion: ($) => iseq("---", ANYTHING),
+
     addition: ($) =>
       choice(
         iseq("+", optional(ANYTHING)),
         iseq("++", optional(ANYTHING)),
         iseq("+++"),
-        iseq("++++", optional(ANYTHING))
+        iseq("++++", optional(ANYTHING)),
+        iseq(">", optional(ANYTHING))
       ),
     deletion: ($) =>
       choice(
         iseq("-", optional(ANYTHING)),
         iseq("--", optional(ANYTHING)),
         iseq("---"),
-        iseq("----", optional(ANYTHING))
+        iseq("----", optional(ANYTHING)),
+        iseq("<", optional(ANYTHING))
       ),
+    change: ($) => iseq("!", optional(ANYTHING)),
 
-    context: ($) => token(prec(-1, ANYTHING)),
+    context: ($) => iseq(" ", optional(ANYTHING)),
     comment: ($) => iseq("#", optional(ANYTHING)),
+    special: ($) => iseq("\\", optional(ANYTHING)),
+    unrecognized: ($) => token(prec(-1, ANYTHING)),
 
     linerange: ($) => /[-\+]\d+(,\d+)?/,
     filename: ($) => repeat1(/\S+/),
-    commit: ($) => /[a-f0-9]{7,40}/,
+    commit: ($) => /[a-f0-9]{4,64}/,
     mode: ($) => /\d+/,
   },
 });
